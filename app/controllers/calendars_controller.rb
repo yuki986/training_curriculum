3d52ade938379fe75108dd743d4d2fdda37964ec
yuki986 <yuki.kuribayashi8@gmail.com>
2021-01-07 18:06:18 +0900

    Revert "Merge pull request #9 from yuki986/second_training"
    This reverts commit d3fec8e11a4b1177612c96dde81bfb77af94c059, reversing
    changes made to f4f785fb6a83a97df42ce8c65a2f4c717707fcbf.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,6 +8,7 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
+    binding.pry
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -15,7 +16,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def get_week
@@ -34,13 +35,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-
-      wday_num = Date.today.wday + x
-      if wday_num > 7
-        wday_num = wday_num - 7
-      end
-
-      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday: wdays[(@todays_date + x).wday]}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
 
