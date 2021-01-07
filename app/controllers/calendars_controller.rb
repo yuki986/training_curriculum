6028d562dc06da98b5a5576b27dacbfdbf20d7b7
yuki986 <yuki.kuribayashi8@gmail.com>
2021-01-07 15:28:24 +0900

    Revert "Issue6"
    This reverts commit 1eb38ea3538342eae5f6157d74e86e34e34e03e9.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,13 +34,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-
-      wday_num = Date.today.wday + x
-      if wday_num > 7
-        wday_num = wday_num - 7
-      end
-
-      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday: wdays[(@todays_date + x).wday]}
+      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans}
       @week_days.push(days)
     end
 
